9da7c84657a3a50ac1583ec6efa9e4c4e2db1be4
Dipak Sarkar <hello@dipaksarkar.in>
2024-04-15 00:53:52 +0530

    Clean up the project

--- src/index.js
+++ src/index.js
@@ -61,14 +61,4 @@ export default (editor, opts = {}) => {
     command: 'open-pages',
     togglable: false
   })
-
-  // TODO Remove
-  editor.on('load', () =>
-    editor.addComponents(
-      `<div style="margin:100px; padding:25px;">
-            Content loaded from the plugin
-        </div>`,
-      { at: 0 }
-    )
-  )
 }
