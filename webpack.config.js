e893560612e666079c55ec3cf2ee83fb066404a2
Dipak Sarkar <hello@dipaksarkar.in>
2024-04-24 01:21:03 +0530

    Build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-// webpack.config.js
+const path = require('path')
 
 module.exports = function ({ config }) {
   return {
@@ -6,6 +6,20 @@ module.exports = function ({ config }) {
     module: {
       rules: [
         ...config.module.rules,
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-transform-runtime', // Plugin for async/await
+                '@babel/plugin-transform-regenerator' // Plugin for async/await
+              ]
+            }
+          }
+        },
         {
           test: /\.scss$/,
           use: ['style-loader', 'css-loader', 'sass-loader']
